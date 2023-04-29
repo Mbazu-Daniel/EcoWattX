f48d6ca855e3920e20f283ee4f37c584777f675c
CodingCas <casweeno2000@gmail.com>
2023-04-29 21:44:45 +0100

    WIP: Hero section completed

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import React from 'react'
+import solar from "../../public/solar.svg"
+
+ const Hero = () => {
+    return (
+      <div className="hero-banner relative bg-cover bg-center">
+        {/* <div className="absolute inset-0 bg-black/75"></div> */}
+        
+        <div className="pt-24 pb-20"  style={{ backgroundImage: "url('/solar.svg')" }}>
+          <div className="w-11/12 lg:w-11/12 mx-auto">
+              <div className="flex gap-5 flex-col lg:flex-row">
+                  <div className="w-full lg:w-3/3 space-y-10 text-center">
+                    <h2 className="text-4xl lg:text-5xl font-bold font-inter">
+                      <span className="text-[#374151] dark:text-white mb-1">Step into the Future of Energy with EcowattX</span>
+                    </h2>
+
+                    <p className="text-normal font-light lg:text-xl tracking-widest text-white">
+                      Discover a new era of clean, affordable, and sustainable energy solutions, Experience <br /> 
+                      cutting-edge technology and innovation in the world of energy, Take control of your <br /> 
+                      energy consumption and help build a brighter future for all.
+                    </p>
+
+                    <div className="w-1/3 mx-auto">
+                      <div className="flex-col lg:flex-row flex gap-6 justify-center text-center mt-20">
+                          <a href="https://qafv61lp0l0.typeform.com/to/gQK4IXCg" target="_blank" rel="noreferrer" className="flex-1 bg-[transparent] border-2 border-[#EAEAEA] dark:border-[#99999966] text-black dark:text-white py-3 px-5 rounded transition-all">Staking</a>
+                          
+                          <a href="https://guildaudits.medium.com/" target="_blank" rel="noreferrer" className="flex-1 bg-[#51A64C] text-white py-3 px-5 rounded transition-all">Get started</a>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default Hero;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
-import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Header />
+      <Hero />
     </>
   );
 }
