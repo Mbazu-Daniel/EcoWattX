bfb2a7661771ade0fc64d0565f34b346f5d5fdf2
Olumide Micheal <olumidemicheal7@gmail.com>
2023-05-24 10:17:59 +0100

    fixed about

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 const About = () => {
   return (
     <div className='overflow-x-hidden'>
-        <h3 className="text-center mb-10 text-4xl mt-24">About us</h3>
+        <h3 className="text-center mb-10 text-4xl mt-24 font-bold">About us</h3>
 
         <div className="bg-[#F5FFF6]">
             <div className="w-10/12 mx-auto pt-20 pb-32" id="about">
                 <div className="flex flex-col lg:flex-row gap-20 items-center mb-24">
                     <div className="w-full lg:w-3/5 space-y-6">
-                        <h3 className="text-3xl font-semibold tracking-wider text-black">Decentralized Energy Generation and Sharing</h3>
-                        <p className="tracking-wide font-light text-lg leading-8 dark:text-[#999999]">
+                        <h3 className="text-3xl font-semibold tracking-wider font-space-grotesk text-black">Decentralized Energy Generation and Sharing</h3>
+                        <p className="tracking-wide font-light text-lg leading-[28px] text-[#000000]">
                             EcowattX is a blockchain-powered platform that allows households to generate energy from renewable sources and sell their excess energy back to the grid. It promotes decentralized energy sharing, cuts energy costs for low-income households, and encourages sustainable practices.
-
-                            <br />
-                            <br />
                         </p>
                     </div>
 
@@ -23,30 +20,24 @@ const About = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col lg:flex-row gap-20 items-center mb-24">
+                <div className="flex flex-col lg:flex-row sm:flex-col-reverse gap-20 items-center mb-24">
                     <div className="w-full lg:w-2/5">
                         <img src="/mask_group.png" alt="Token Reward" />
                     </div>
 
                     <div className="w-full lg:w-3/5 space-y-6">
-                        <h3 className="text-3xl font-semibold tracking-wider text-black">Token Reward Program for Sustainable Behavior</h3>
-                        <p className="tracking-wide font-light text-lg leading-8 dark:text-[#999999]">
+                        <h3 className="text-3xl font-semibold tracking-wider font-space-grotesk text-black">Token Reward Program for Sustainable Behavior</h3>
+                        <p className="tracking-wide font-light text-lg leading-[28px] text-[#000000]">
                             The EcowattX platform incentivizes sustainable behavior by offering token rewards to households that adopt renewable energy sources or use less energy. It aims to encourage energy efficiency, promote community resilience, and foster the adoption of renewable energy sources.
-
-                            <br />
-                            <br />
                         </p>
                     </div>
                 </div>
 
                 <div className="flex flex-col lg:flex-row gap-20 items-center">
                     <div className="w-full lg:w-3/5 space-y-6">
-                        <h3 className="text-3xl font-semibold tracking-wider text-black">Reducing Energy Costs for Low-Income Households</h3>
-                        <p className="tracking-wide font-light text-lg leading-8 dark:text-[#999999]">
+                        <h3 className="text-3xl font-semibold tracking-wider font-space-grotesk text-black">Reducing Energy Costs for Low-Income Households</h3>
+                        <p className="tracking-wide leading-[28px] font-light text-lg text-[#000000]">
                             We seek to promote sustainability, reduce energy costs for low-income households, and empower them to adopt renewable energy sources. Its goal is to create a more equitable and sustainable energy future for all.
-
-                            <br />
-                            <br />
                         </p>
                     </div>
 
